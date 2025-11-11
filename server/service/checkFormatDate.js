62609b29133d5750e8cfb2f36bf16d3e631d4071
TU CAM KIET <camkiettu@gmail.com>
2025-11-11 12:47:31 +0700

    main

--- server/service/checkFormatDate.js
+++
@@ -1,8 +0,0 @@
-const moment = require("moment");
-function isValidDateFormat(input) {
-  const format = "DD-MM-YYYY";
-
-  return moment(input, format, true).isValid();
-}
-
-module.exports = isValidDateFormat;
